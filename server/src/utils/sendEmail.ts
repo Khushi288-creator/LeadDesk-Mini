04a79b6bbfe0657d06d796cecb8a5bc04d4e95ef
Khushi <ktm407301@gmail.com>
2026-07-26 16:52:54 +0530

    fix email sending by forcing ipv4 connection

--- server/src/utils/sendEmail.ts
+++ server/src/utils/sendEmail.ts
@@ -1,12 +1,17 @@
 import nodemailer from "nodemailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
+
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_APP_PASSWORD,
   },
-});
+  family: 4, // 👈 ye line force karegi IPv4 use karne ke liye
+} as SMTPTransport.Options);
 
 export const sendStatusUpdateEmail = async (
   toEmail: string,
@@ -31,6 +36,7 @@ export const sendStatusUpdateEmail = async (
         </div>
       `,
     });
+    console.log(`Status email sent to ${toEmail}`);
   } catch (err) {
     console.log("Email send failed:", err);
   }
